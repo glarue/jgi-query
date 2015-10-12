316eebcd01356023febbca6a1fc9c871bff1246b
Graham Larue <egrahamlarue@gmail.com>
2015-10-12 11:57:03 -0700

    Make shorten_timestamp() parse strings with "UTC" as well

--- jgi-query.py
+++ jgi-query.py
@@ -296,7 +296,7 @@ def shorten_timestamp(time_string):
     """
     # Remove platform-dependent timezone substring
     # of the general form "xxT"
-    tz_pattern = re.compile("\s[A-Z]{2}T\s")
+    tz_pattern = re.compile("\s[A-Z]{3}\s")
     time_string = tz_pattern.sub(" ", time_string)
 
     # Get the desired time info
