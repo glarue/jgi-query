b80eea1ec0ed6e20b4b7e9ad6f319183425d3829
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 09:19:50 +0000

    Adding tarfile member sanitization to extractall()

--- jgi-query.py
+++ jgi-query.py
@@ -332,7 +332,26 @@ def extract_file(file_path, keep_compressed=False):
                 destination = dir_name
             else:  # single file, extract into working directory
                 destination = "."
-            f.extractall(destination)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, destination)
     elif re.search(gz_pattern, file_path):
         opener, mode, ext = endings_map["gz"]
         # out_name = file_path.rstrip(ext)
